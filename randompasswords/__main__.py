559d9474db4fe3ca2c05b9e241ee9b7f711942e9
Joseph Jung <josephjung12@gmail.com>
2019-05-31 13:54:44 -0700

    Added GUI components

--- randompasswords/__main__.py
+++ randompasswords/__main__.py
@@ -1,8 +1,6 @@
 #__main__.py for randompasswords
 
-from randompasswords.passwordgenerator import *
-from faker import Faker
-from InputFrame import *
+from randompasswords.InputFrame import *
 import wx
 
 def main():
@@ -11,23 +9,6 @@ def main():
     app = wx.App()
     frame = InputFrame()
     app.MainLoop()
-"""
-    with open("randompasswords.txt", "w+") as fh:    # open or create a textfile to store inputted keywords
-        inputKeywords(fh)
-        fh.write(fake.email() + "\n")
-        fh.write(fake.address() + "\n")
-        fh.write(str(fake.longitude()) + "\n")
-        fh.write(str(fake.latitude()) + "\n")
-
-    with open("randompasswords.txt", "r+") as fh:    # takes keywords and puts them in data structures to prepare for generating pws
-        storage = storeKeywords(fh)
-
-    passwords = generatePasswords(storage)
-    print("\nPrinting passwords now\n\n")
-
-    with open("randompasswords.txt", "a") as fh:
-        storePasswords(fh, passwords)
-"""
 
 if __name__ == "__main__":
     main()
